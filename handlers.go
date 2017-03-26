0ef352df677cac3f382ae3136709433557866b94
Ansel Rosenberg <ansel01@gmail.com>
2017-03-26 15:55:01 -0700

    Refactor - split into main, routes, handlers

--- handlers.go
+++ handlers.go
@@ -2,13 +2,12 @@ package main
 
 import (
 	"fmt"
-	"os"
-	"log"
-	"net/http"
-	"database/sql"
 	"encoding/json"
-	_ "github.com/go-sql-driver/mysql"
+	"net/http"
 	"github.com/gorilla/mux"
+	_ "github.com/go-sql-driver/mysql"
+	"database/sql"
+	"os"
 )
 
 var db *sql.DB
@@ -23,18 +22,26 @@ type Player struct {
 	Weight int `json:"weight"`
 }
 
-/*
-type appError struct {
-	Error error
-	Message string
-	Code int
-}
-*/
 
 func PlayerHandler(w http.ResponseWriter, r *http.Request) {
 	vars := mux.Vars(r)
 	var name, number, team, pos string
 	var height, weight int
+
+
+	  datasource := os.Getenv("USER") + ":@/" + os.Getenv("DB_NAME")
+	  db, err = sql.Open("mysql", datasource)
+
+	  if err != nil {
+	      fmt.Println(err)
+	  }
+
+	  defer db.Close()
+
+	  err = db.Ping()
+	  if err != nil {
+	      fmt.Println(err)
+	  }
 
 	row := db.QueryRow("SELECT name, number, team, pos, height, weight FROM nba_player WHERE id=?;", vars["player_id"])
 	err = row.Scan(&name, &number, &team, &pos, &height, &weight)
@@ -52,25 +59,4 @@ func PlayerHandler(w http.ResponseWriter, r *http.Request) {
 
 	w.Header().Set("Content-Type", "application/json")
 	w.Write(jsonResponse)
-}
-
-func main() {
-	datasource := os.Getenv("USER") + ":@/" + os.Getenv("DB_NAME")
-	db, err = sql.Open("mysql", datasource)
-
-	if err != nil {
-		fmt.Println(err)
-	}
-
-	defer db.Close()
-
-	err = db.Ping()
-	if err != nil {
-		fmt.Println(err)
-	}
-
-	r := mux.NewRouter()
-	r.HandleFunc("/player/{player_id}", PlayerHandler)
-
-	log.Fatal(http.ListenAndServe(":8080", r))
 }
---
+++ routes.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"net/http"
+	"github.com/gorilla/mux"
+)
+
+type Route struct {
+	Name        string
+	Method      string
+	Pattern     string
+	HandlerFunc http.HandlerFunc
+}
+
+type Routes []Route
+
+func NewRouter() *mux.Router {
+
+	router := mux.NewRouter().StrictSlash(true)
+	for _, route := range routes {
+		handler := Logger(route.HandlerFunc, route.Name)
+
+		router.
+			Methods(route.Method).
+			Path(route.Pattern).
+			Name(route.Name).
+			Handler(handler)
+	}
+
+	return router
+}
+
+var routes = Routes{
+	Route{
+		"Player",
+		"GET",
+		"/player/{player_id}",
+		PlayerHandler,
+	},
+}
