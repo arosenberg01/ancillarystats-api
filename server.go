280122ca6fa13cbddcc3d7d4307905332db4366e
Ansel Rosenberg <ansel01@gmail.com>
2017-03-26 14:36:38 -0700

    Add mux router

--- server.go
+++ server.go
@@ -3,10 +3,12 @@ package main
 import (
 	"fmt"
 	"os"
+	"log"
 	"net/http"
 	"database/sql"
 	"encoding/json"
 	_ "github.com/go-sql-driver/mysql"
+	"github.com/gorilla/mux"
 )
 
 var db *sql.DB
@@ -21,12 +23,20 @@ type Player struct {
 	Weight int `json:"weight"`
 }
 
-func handler(w http.ResponseWriter, r *http.Request) {
+/*
+type appError struct {
+	Error error
+	Message string
+	Code int
+}
+*/
+
+func PlayerHandler(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
 	var name, number, team, pos string
 	var height, weight int
 
-	player_id := r.URL.Path[1:]
-	row := db.QueryRow("SELECT name, number, team, pos, height, weight FROM nba_player WHERE id=?;", player_id)
+	row := db.QueryRow("SELECT name, number, team, pos, height, weight FROM nba_player WHERE id=?;", vars["player_id"])
 	err = row.Scan(&name, &number, &team, &pos, &height, &weight)
 
 	if err != nil {
@@ -46,23 +56,21 @@ func handler(w http.ResponseWriter, r *http.Request) {
 
 func main() {
 	datasource := os.Getenv("USER") + ":@/" + os.Getenv("DB_NAME")
-
 	db, err = sql.Open("mysql", datasource)
+
 	if err != nil {
-		panic(err.Error())
+		fmt.Println(err)
 	}
 
 	defer db.Close()
 
 	err = db.Ping()
 	if err != nil {
-		panic(err.Error())
+		fmt.Println(err)
 	}
 
-	http.HandleFunc("/", handler)
-	err := http.ListenAndServe(":8080", nil)
+	r := mux.NewRouter()
+	r.HandleFunc("/player/{player_id}", PlayerHandler)
 
-	if err != nil {
-		panic(err.Error())
-	}
+	log.Fatal(http.ListenAndServe(":8080", r))
 }
