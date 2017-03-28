6b905c256ff5de0c79a47247ded648d7b357c630
Ansel Rosenberg <ansel01@gmail.com>
2017-03-27 23:12:27 -0700

    Add handler for category leaders retrieval

--- handlers.go
+++ handlers.go
@@ -8,6 +8,7 @@ import (
 	_ "github.com/go-sql-driver/mysql"
 	"database/sql"
 	"os"
+	"log"
 )
 
 var db *sql.DB
@@ -22,6 +23,10 @@ type Player struct {
 	Weight int `json:"weight"`
 }
 
+var leaderCategories = map[string]bool {
+	"pts": true,
+}
+
 
 func PlayerHandler(w http.ResponseWriter, r *http.Request) {
 	vars := mux.Vars(r)
@@ -29,19 +34,19 @@ func PlayerHandler(w http.ResponseWriter, r *http.Request) {
 	var height, weight int
 
 
-	  datasource := os.Getenv("USER") + ":@/" + os.Getenv("DB_NAME")
-	  db, err = sql.Open("mysql", datasource)
+	datasource := os.Getenv("USER") + ":@/" + os.Getenv("DB_NAME")
+	db, err = sql.Open("mysql", datasource)
 
-	  if err != nil {
-	      fmt.Println(err)
-	  }
+	if err != nil {
+	  fmt.Println(err)
+	}
 
-	  defer db.Close()
+	defer db.Close()
 
-	  err = db.Ping()
-	  if err != nil {
-	      fmt.Println(err)
-	  }
+	err = db.Ping()
+	if err != nil {
+	  fmt.Println(err)
+	}
 
 	row := db.QueryRow("SELECT name, number, team, pos, height, weight FROM nba_player WHERE id=?;", vars["player_id"])
 	err = row.Scan(&name, &number, &team, &pos, &height, &weight)
@@ -57,6 +62,46 @@ func PlayerHandler(w http.ResponseWriter, r *http.Request) {
 		fmt.Println(err)
 	}
 
-	w.Header().Set("Content-Type", "application/json")
+	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
+	w.WriteHeader(http.StatusOK)
 	w.Write(jsonResponse)
+}
+
+func LeadersHandler(w http.ResponseWriter, r *http.Request) {
+	var (
+		player_id string
+		cat_avg string
+		category string
+	)
+	vars := mux.Vars(r)
+	datasource := os.Getenv("USER") + ":@/" + os.Getenv("DB_NAME")
+
+	db, err = sql.Open("mysql", datasource)
+
+	if err != nil {
+		fmt.Println(err)
+	}
+	
+	_, ok := leaderCategories[vars["category"]]
+
+	if ok {
+		query := "SELECT player_id, AVG(" + category + ") AS cat_avg FROM nba_game GROUP BY player_id ORDER BY cat_avg DESC LIMIT 10"
+		rows, err := db.Query(query)
+
+		defer rows.Close()
+
+		for rows.Next() {
+			err := rows.Scan(&player_id, &cat_avg)
+
+			if err != nil {
+				log.Fatal(err)
+			}
+
+			log.Println(player_id, cat_avg)
+		}
+
+		if err = rows.Err(); err != nil {
+			log.Fatal(err)
+		}
+	}
 }
