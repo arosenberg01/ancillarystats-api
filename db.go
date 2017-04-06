04b4ee52a2396dc881909184ee12b54a60ec27cc
Ansel Rosenberg <ansel01@gmail.com>
2017-04-05 21:02:37 -0700

    Panic if db connection open fails

--- db.go
+++ db.go
@@ -4,20 +4,10 @@ import (
 	_ "database/sql"
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/jmoiron/sqlx"
-	_ "database/sql"
 )
 
-//type Datastore interface {
-//	Player() (Player, error)
-//	Leaders() ([]CategoryLeaders, error)
-//}
-//
-//type DB struct {
-//	*sqlx.DB
-//}
-
 func NewDB(dataSource string) (*sqlx.DB) {
-	db, _ := sqlx.Open("mysql", dataSource)
+	db := sqlx.MustConnect("mysql", dataSource)
 
 	return db
 }
