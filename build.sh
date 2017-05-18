b83c62b33010c3eb4d93fd1942a304fd74200a0c
Ansel Rosenberg <ansel01@gmail.com>
2017-05-18 16:30:18 -0400

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,4 +1,5 @@
 go get github.com/gorilla/mux
 go get github.com/jmoiron/sqlx
 go get github.com/go-sql-driver/mysql
-go build -o bin/application application.go
+go build -o bin/application *.go
+rm application.go
