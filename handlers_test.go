cadb4c6d152b0fa1470aeb5afd0c5741c5fcf02a
Ansel Rosenberg <ansel01@gmail.com>
2017-05-21 11:25:01 -0400

    Add unit tests

---
+++ handlers_test.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"testing"
+	"reflect"
+)
+
+func TestStrMapKeys(t *testing.T) {
+	sample := map[string]string {
+		"key_a": "0",
+		"key_b": "1",
+		"key_c": "2",
+	}
+	expected := []string{"key_a", "key_b", "key_c"}
+	result, _ := StrMapKeys(sample)
+
+	eq := reflect.DeepEqual(expected, result)
+
+	if !eq {
+		t.Error("Map keys were not returned")
+	}
+}
