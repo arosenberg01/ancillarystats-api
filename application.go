76e1468f8dcf04642ede3b534b72d46af89012c2
Ansel Rosenberg <ansel01@gmail.com>
2017-05-27 21:31:12 -0400

    Add Negroni

--- application.go
+++ application.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"github.com/urfave/negroni"
 )
 
 type Env struct {
@@ -12,15 +13,23 @@ type Env struct {
 }
 
 func main() {
-	datasource := fmt.Sprintf("%s:%s@tcp(%s)/%s", os.Getenv("DB_USER"), os.Getenv("DB_PW"), os.Getenv("DB_INSTANCE"), os.Getenv("DB_NAME"))
+	datasource := fmt.Sprintf("%s:%s@tcp(%s)/%s", os.Getenv("DB_USER"), os.Getenv("DB_PW"),
+		os.Getenv("DB_INSTANCE"), os.Getenv("DB_NAME"))
 	db := NewDB(datasource)
 	env := &Env{db}
+
 	router := NewRouter(env)
+
+	n := negroni.New()
+	n.UseHandler(router)
+
 
 	port := ":5000"
 	if envPort := os.Getenv("PORT"); envPort != "" {
 		port = ":" + envPort
 	}
 
-	log.Fatal(http.ListenAndServe(port, router))
+	//log.Fatal(http.ListenAndServe(port, router))
+	log.Fatal(http.ListenAndServe(port, n))
+
 }
