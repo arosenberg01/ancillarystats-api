0693015503a39ee60d13d358a189514940e2e11c
Ansel Rosenberg <ansel01@gmail.com>
2017-06-02 15:55:20 -0400

    Update build.sh with negroni

--- build.sh
+++ build.sh
@@ -1,5 +1,6 @@
 go get github.com/gorilla/mux
 go get github.com/jmoiron/sqlx
 go get github.com/go-sql-driver/mysql
+go get github.com/urfave/negroni
 go build -o bin/application *.go
 rm application.go
